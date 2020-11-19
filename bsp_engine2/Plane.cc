fc73d83fe0a403656b0386e4a1718d91ca471ae0
dbusato <davidedbusato@gmail.com>
2020-11-19 09:45:03 +0100

    Fixing makefiles and sources

--- bsp_engine2/Plane.cc
+++ bsp_engine2/Plane.cc
@@ -1,5 +1,7 @@
 #include "Plane.h"
 
+const double Plane::EPS = 0.001;
+
 Plane::Plane(const Wall& wall) {
   double x1 = wall.points[0].x;
   double y1 = wall.points[0].y;
@@ -16,9 +18,9 @@ Plane::Position Plane::evaluate_position(const Vector2d& point) {
   
   double val = a * point.x + b * point.y + c;
   
-  if (val > eps)
+  if (val > EPS)
     return IN_FRONT;
-  else if (val < -eps)
+  else if (val < -EPS)
     return IN_BACK;
   else
     return SPANNING;
